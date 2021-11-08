099f3ca4a253042bcc9aaa0692ed9a54b0761d13
Santiago-Montiel <santmontiel@gmail.com>
2021-11-07 19:54:44 -0500

    Actualizacion de cambios de rama

--- app/Http/Controllers/ResidenteController.php
+++ app/Http/Controllers/ResidenteController.php
@@ -52,6 +52,30 @@ class ResidenteController extends Controller
     public function store(Request $request)
     {
         //
+
+        $campos=[
+            'NUMERO_IDENTIFICACION'=>'required|Integer|max:10|unique',
+            'ID_TIPO_IDENTIFICACION'=>'required',
+            'NOMBRE'=>'required|String|max:45',
+            'APELLIDO'=>'required|String|max:45',
+            'SEXO'=>'required',
+            'TELEFONO'=>'Integer|max:9',
+            'CELULAR1'=>'required|Integer|max:9',
+            'CELULAR2'=>'Integer|max:9',
+            'CORREO_ELECTRONICO'=>'required|String|max:100',
+            'NUMERO_APTO'=>'required',
+            'BLOQUE'=>'required'
+        ];
+        $mensaje=[
+            'required'=>'El :attribute es requerido',
+            'unique'=>'El :attribute ya existe',
+            'Integer'=> 'El :attribute debe ser numerico',
+            
+        ];
+    
+        $this->validate($request, $campos, $mensaje);
+
+
         $datosResidente = request()->except('_token','NUMERO_APTO','BLOQUE');
         $datosResidentex = request()->except('_token');
         
--- resources/views/residente/form.blade.php
+++ resources/views/residente/form.blade.php
@@ -1,3 +1,14 @@
+@if(count($errors)>0)
+
+<div class="alert alert-danger" role="alert">
+    <ul>
+        @foreach($errors->all() as $error)
+             <li>{{$error}}</li>
+            @endforeach
+    </ul>
+</div>
+
+@endif
 
 <h2>{{ $modo }} residente</h2>
 
