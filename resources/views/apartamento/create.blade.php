67ec4f3e7e901cf99643a95661cf637a83e43a2d
Santiago-Montiel <santmontiel@gmail.com>
2021-11-18 16:21:31 -0500

    Arreglo de stilos, formularios apartamento

--- resources/views/apartamento/create.blade.php
+++ resources/views/apartamento/create.blade.php
@@ -19,26 +19,55 @@
     <!-- start: Content -->
 
 
-<h2>Registrar apartamento</h2>
+    <div id="content">
+                <div class="panel box-shadow-none content-header">
+                  <div class="panel-body">
+                    <div class="col-md-12">
+                        <h3 class="animated fadeInLeft">Formulario de apartamento</h3>
+                        <p class="animated fadeInDown">
+                          Admin <span class="fa-angle-right fa"></span> Registar Apartamento
+                        </p>
+                    </div>
+                </div>
+    </div>
+
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
 
 @csrf
-<form action="{{url('/NumeroApto')}} " method="post">
+<form action="{{url('/NumeroApto')}} " method="post" class="form-element">
 @csrf
-    <div class="mb-3">
-        <label for="NUMERO_APTO" class="form-label">Numero de apartamento</label>
-        <input type="number" name="NUMERO_APTO" maxlength="150" id="NUMERO_APTO" class="form-control" value="" tapindex="1" required>
-        <input type="submit" class="btn btn-primary" value="Registrar apartamento" tapindex="4">
-    </div>
+    <div class="col-md-12 padding-0">
+        <div class="col-md-8">
+            <div class="panel form-element-padding">
+                    <div class="panel-heading">
+                        <h4>Nuevo Numero de Apartamento</h4>
+                    </div>
 
+                    <div class="form-group">
+                        <label for="NUMERO_APTO" class="font-weight-bold">Numero de apartamento <span class="text-danger">*</span> </label>
+                        <input type="number" name="NUMERO_APTO" maxlength="150" id="NUMERO_APTO" class="form-control" value="" tapindex="1" required>
+                        <input type="submit" class="btn btn-outline-success" value="Registrar apartamento" tapindex="4">
+                    </div>
+            </div>
+        </div>
+    </div>
 </form>
 
-<form action="{{url('/bloque')}} " method="post">
+<form action="{{url('/bloque')}} " method="post" class="form-element">
 @csrf
-    <div class="mb-3">
-        <label for="BLOQUE" class="form-label">Bloque</label>
-        <input type="text" name="BLOQUE" maxlength="5" id="BLOQUE" class="form-control" value="" tapindex="2" required>
-        <input type="submit" class="btn btn-primary" value="Registrar bloque" tapindex="4">
+    <div class="col-md-12 padding-0">
+            <div class="col-md-8">
+                <div class="panel form-element-padding">
+                    <div class="panel-heading">
+                        <h4>Nuevo Bloque</h4>
+                    </div>
+                    <div class="form-group">
+                        <label for="BLOQUE" class="font-weight-bold">Bloque <span class="text-danger">*</span> </label>
+                        <input type="text" name="BLOQUE" maxlength="5" id="BLOQUE" class="form-control" value="" tapindex="2" required>
+                        <input type="submit" class="btn btn-outline-success" value="Registrar bloque" tapindex="4">
+                    </div>
+                </div>
+        </div>
     </div>
 </form>
 @include('apartamento.form',['modo'=>'Registrar'])
--- resources/views/apartamento/edit.blade.php
+++ resources/views/apartamento/edit.blade.php
@@ -2,6 +2,17 @@
 <h2>Editar apartamento</h2>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
 
+<div id="content">
+                <div class="panel box-shadow-none content-header">
+                  <div class="panel-body">
+                    <div class="col-md-12">
+                        <h3 class="animated fadeInLeft">Formulario de Apartamento</h3>
+                        <p class="animated fadeInDown">
+                          Admin <span class="fa-angle-right fa"></span> Editar Apartamento
+                        </p>
+                    </div>
+                  </div>
+</div>
 
 <form action="{{ url('/apartamento/'.$apartamento->ID_APARTAMENTO)}}" method="POST">
 @csrf
