4224be88e44e207d4f0195128a8ee251fba98bfe
JuanMoreno12 <juansemorenosalcedo92@gmail.com>
2021-12-02 13:10:49 -0500

    Cambio de idioma y ultimos detalles en ls estilos

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -35,12 +35,15 @@ class HomeController extends Controller
     }
 
     public function admin($id=null)
-    {
-        $asignacionList=DB::select('select detalle_asignaciones.apartamento_id, detalle_asignaciones.vehiculo_id, detalle_asignaciones.parqueadero_id, detalle_asignaciones.inicio, detalle_asignaciones.fin
+    {$asignacion=Detalle_asignacion::all();
+        $asignacionList=DB::select('select ID_APARTAMENTO,BLOQUE.BLOQUE,numeroapartamento.NUMERO_APTO,vehiculos.placa, detalle_asignaciones.vehiculo_id, detalle_asignaciones.parqueadero_id, detalle_asignaciones.inicio, detalle_asignaciones.fin
         from detalle_asignaciones 
-        inner join apartamento on detalle_asignaciones.apartamento_id =apartamento.ID_APARTAMENTO
-        inner join vehiculos on detalle_asignaciones.vehiculo_id=vehiculos.id
-        inner join parqueaderos on detalle_asignaciones.parqueadero_id=parqueaderos.id');
+                inner join apartamento on detalle_asignaciones.apartamento_id =apartamento.ID_APARTAMENTO
+                inner join vehiculos on detalle_asignaciones.vehiculo_id=vehiculos.id
+                inner join parqueaderos on detalle_asignaciones.parqueadero_id=parqueaderos.id
+                inner join  bloque ON apartamento.BLOQUE=bloque.id
+                INNER JOIN numeroapartamento ON apartamento.NUMERO_APTO=numeroapartamento.id
+        WHERE parqueadero_id=parqueadero_id');
         
         $parqueadero=Parqueadero::all();
         $cantidadres=DB::select('select ResidentesTotales() as residente');
@@ -61,7 +64,7 @@ class HomeController extends Controller
          }else{
             $asignacion=Detalle_asignacion::findOrFail($id);
          }
-        return view('Dashboards.Dashboard_admin', compact('cantidadveh','vista','cantidadres','parqueadero','parqueaderoV','vehiculo','NumeroApto','asignacion','vis','asignacionList'));
+        return view('Dashboards.Dashboard_admin', compact('cantidadveh','vista','cantidadres','asignacion','parqueadero','parqueaderoV','vehiculo','NumeroApto','asignacion','vis','asignacionList'));
 
     }
 
@@ -80,13 +83,20 @@ class HomeController extends Controller
         if (auth()->user()->ID_TIPO_USUARIO  == 1) {
             return redirect('/admin');
         } elseif (auth()->user()->ID_TIPO_USUARIO  == 2) {
-            return redirect('/secretatia');
+            return redirect('/admin');
         } elseif (auth()->user()->ID_TIPO_USUARIO  == 3) {
-            return redirect('/guarda');
+            return redirect('/admin');
         } else {
             return redirect('/');
         }
     }
+        public function delete(Request $request, $parqueadero_id)
+    {
+        DB::delete('delete from detalle_asignaciones where parqueadero_id='.$parqueadero_id);
+        DB::update('update parqueaderos set OCUPADO = 0 where id='.$parqueadero_id);
+        return redirect('/admin');
+    }
+    
 
 
 }
--- app/Http/Controllers/datosController.php
+++ app/Http/Controllers/datosController.php
@@ -12,6 +12,10 @@ use Illuminate\Support\Facades\DB;
 
 class datosController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
     public function index()
     {
         return view('home');
