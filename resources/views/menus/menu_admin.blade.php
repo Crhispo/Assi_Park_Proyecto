b3d4ee1bde1020048dbf7416d7ef42e0352999c4
JuanMoreno12 <juansemorenosalcedo92@gmail.com>
2021-12-03 11:58:26 -0500

    :(

--- resources/views/menus/menu_admin.blade.php
+++ resources/views/menus/menu_admin.blade.php
@@ -70,7 +70,9 @@
 
       <li class="ripple">
         <a  href="/Datos">
-          <span class="icon-chart icons" style="font-size:1em;"></span> Reportes<span class="fa-angle-right fa right-arrow text-right"></span>
+
+          <span class="icon-chart icons" style="font-size:1em;"></span> Estadísticas
+       
         </a>
       </li>
       
@@ -94,6 +96,7 @@
       <li class="active ripple">
         <a href="/admin"><span class="icon-home icons" style="font-size:1em;"></span> Inicio
           <span class="fa-angle-right fa right-arrow text-right"></span>
+
         </a>
         <ul class="nav nav-list tree">
         </ul>
